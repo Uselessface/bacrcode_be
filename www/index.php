a95de6ad1e71257e4d54a324421db87497f95ebb
Uselessface <60576572+Uselessface@users.noreply.github.com>
2024-09-27 14:14:04 +0400

    added file saving

--- www/index.php
+++ www/index.php
@@ -1,2 +1,11 @@
-<?php    phpinfo() ?>
+<?php
+
+    var_dump($_POST, $_REQUEST, file_get_contents("php://input"));
+    $data = $_POST;
+    $serializedData = serialize($data);
+
+    $filename = __DIR__ . '/barcodes.txt';
+
+    file_put_contents($filename, $serializedData, FILE_APPEND|LOCK_EX)
+?>
  
